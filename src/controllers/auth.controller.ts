887ab8b9f948cfc09222805b60cc8f03ffbf0ed6
KabitaAdk <kabitaadk55@gmail.com>
2026-01-03 19:12:14 +0545

    get single user details

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -32,3 +32,16 @@ export const getallUser = async (req:Request, res:Response) => {
     const users = await prisma.user.findMany({});
     res.json({message: "User fetch successfully", data:users} );
 }
+
+export const getsingleUser = async (req:Request, res:Response) => {
+    const { email } = req.params;
+    const user = await prisma.user.findUnique({
+        where: {
+            email: String(email),
+        }
+    });
+    if (!user){
+        return res.status(404).json({message: "User not found"});
+    }
+    res.json({message: "User fetched cuccessfully", data: user});
+}
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { register, login, getallUser } from "../controllers/auth.controller";
+import { register, login, getallUser, getsingleUser } from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/register", register);
 //router.get("/users", getallUser);
 router.post("/login", login);
-
+//router.get("/user", getsingleUser);
 
 export default router;
